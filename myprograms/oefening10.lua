c65cfb718c73f195c69a18cb7190d231e57efcdb
Richard Boon <99041876@mydavinci.nl>
2016-09-13 11:24:21 +0200

    Rebuild 10

--- myprograms/oefening10.lua
+++ myprograms/oefening10.lua
@@ -3,30 +3,14 @@ robot_arm:load_level('exercise 11')
 
 robot_arm.speed = 0.999
 
-count = 0
-
-while count ~= 9 do
-  while find ~= 1 do
-    robot_arm:grab()
-    what = robot_arm:scan()
-    if what=="white" then
-      print('[LOG] Find something')
-      find = 1
-    elseif what~="white" then
-      robot_arm:drop()
-      robot_arm:move_right()
-      print('[LOG] Find nothing')
-    end
-  end
-  for i=1, 9 do
+for i=0, 8 do
+  robot_arm:grab()
+  if robot_arm:scan() == "white" then
+    robot_arm:move_right()
+    robot_arm:drop()
+    robot_arm:move_right()
+  elseif robot_arm:scan() ~= "white" then
+    robot_arm:drop()
     robot_arm:move_right()
   end
-  robot_arm:drop()
-  count = count+1
-  find = 0
-  print ("[LOG] Blocks moved: "..count)
-  for i=1, 9 do
-    robot_arm:move_left()
-  end
 end
-print("[LOG] Done!")
