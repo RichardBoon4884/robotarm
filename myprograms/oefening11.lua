11722e8a9253fbfbc58f3ead8d1a208fbcc29608
Richard Boon <99041876@mydavinci.nl>
2016-09-14 09:03:32 +0200

    Rewrite

--- myprograms/oefening11.lua
+++ myprograms/oefening11.lua
@@ -1,33 +1,21 @@
 require 'robot_arm'
 robot_arm:load_level('exercise 11')
 
-find = 0
-count = 0
-
 robot_arm.speed = 0.999
 
-while count ~= 3 do
-  while find ~= 1 do
-    robot_arm:grab()
-    what = robot_arm:scan()
-    if what=="red" then
-      print('[LOG] Find something')
-      find = 1
-    elseif what~="red" then
-      robot_arm:drop()
+for i=0, 8 do
+  robot_arm:grab()
+  if robot_arm:scan() == "red" then
+    for a=1, 8 do
       robot_arm:move_right()
-      print('[LOG] Find nothing')
     end
-  end
-  for i=1, 9 do
+    robot_arm:drop()
+    robot_arm:move_right()
+    for a=1, 8 do
+      robot_arm:move_left()
+    end
+  elseif robot_arm:scan() ~= "red" then
+    robot_arm:drop()
     robot_arm:move_right()
   end
-  robot_arm:drop()
-  count= count+1
-  find = 0
-  print ("[LOG] Blocks moved: "..count)
-  for i=1, 9 do
-    robot_arm:move_left()
-  end
 end
-print("[LOG] Done!")
